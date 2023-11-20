bad6a0c4b8e55f08ffd55d6ab8992b0040efb495
pascaill <147127122+pascaill@users.noreply.github.com>
2023-11-20 17:47:01 +0700

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,7 +1,19 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-class Menu {// kelas Menu yang akan digunakan 
+class Menu implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+// kelas Menu yang akan digunakan 
     //untuk mengelola semua item menu dalam restoran. 
     //Kelas ini harus memiliki atribut berupa sebuah 
     //ArrayList untuk menyimpan semua item menu.
@@ -34,5 +46,70 @@ class Menu {// kelas Menu yang akan digunakan
             }
         }
         return false; // Menu belum ada
+    }
+
+//Menyimpan menu ke dalam file
+    // Metode untuk menulis menu ke file teks
+    public void simpanMenuKeFile(String namaBerkas) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(namaBerkas))) {
+            for (int i = 0; i < menuItems.size(); i++) {
+                MenuItem item = menuItems.get(i);
+                writer.write((i + 1) + ". " + item.getName() + " " + item.getPrice() + "(" + item.getCategory() + ")");
+                writer.newLine();
+            }
+
+            System.out.println("Berhasil Menulis Menu ke  "+namaBerkas);
+        } catch (IOException e) {
+            System.out.println("Gagal Menulis ke File");
+            e.printStackTrace();
+        }
+    }
+
+    public static void buatFile(String namaFile) {
+        File file = new File(namaFile);
+
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File " + namaFile + " berhasil dibuat");
+            } else {
+                System.out.println("File " + namaFile + " sudah ada");
+            }
+        } catch (IOException e) {
+            System.err.println("Gagal membuat file " + namaFile);
+            e.printStackTrace();
+        }
+    }
+
+    //Membaca menu dari file
+    public void bacaMenuDariFile(String namaBerkas) {
+        File file = new File(namaBerkas);
+
+        if (!file.exists()) {
+            System.err.println("File " + namaBerkas + " tidak ditemukan.");
+            return;
+        }
+
+        try ( BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String baris;
+            menuItems = new ArrayList<>();
+
+            while ((baris = bufferedReader.readLine()) != null) {
+                System.out.print("Daftar Menu Makanan dan Minuman\n");
+                System.out.println(" " + baris);
+                
+                
+
+            }
+
+            System.out.println("Berhasil Membaca Menu dari File");
+            
+        for (int i = 0; i < menuItems.size(); i++) {
+            MenuItem item = menuItems.get(i);
+            System.out.print((i + 1) + "." + item.getName() + " " + item.getPrice() + "(" + item.getCategory() + ")" + "\n");
+        }
+        } catch (IOException e) {
+            System.out.println("Gagal Baca File");
+            e.printStackTrace();
+        }
     }
 }
